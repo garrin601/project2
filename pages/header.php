3783e3f35a6d6acf0cd758f42e0e97ec4f2410cd
garrin c saccento <gcs23@afsconnect2.njit.edu>
2017-12-17 21:17:01 -0500

    updated header page, cannot get bootstrap to work correctly

--- pages/header.php
+++ pages/header.php
@@ -9,20 +9,18 @@
 
 <body>
  <header class="container">
-  <div class="row"
-  <nav class="col-sm-10">			        <ul>
+
+
   <li><a href="index.php">Return Home</a></li>
   <li><a href= "index.php?page=accounts&action=all">Show all of your Current Accounts</a></li>
   <li><a href="index.php?page=tasks&action=all">Show all of your Current Tasks</a></li>
-   <li><a href= "index.php?page=userlogin&action=login">Login</a></li>
 
-	</ul>
-      <h1>Hellofdfdafsadfs</h1>
- </na>
- </div>
- </header>
 
- <div class="row text-center">
-<p class="subtitle">Garrin Saccento IS 601 project</p>
- </div>
+
+
+
+
+
+
+<h1> Garrin Saccento IS 601 project</h1>
  </section>
